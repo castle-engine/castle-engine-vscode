dfecfcb7e9e6164e005d80eda875d939c23d4b7b
and3md <and3md@gmail.com>
2023-11-07 13:25:26 +0100

    Add Run, Compile, and Clean buttons to status bar.

--- src/castleStatusBar.js
+++ src/castleStatusBar.js
@@ -5,7 +5,10 @@ class CastleStatusBar {
     constructor(context, castleConfig) {
         this._castleConfig = castleConfig;
         this._context = context;
-        this.createBuildModeSwitch()
+        this.createBuildModeSwitch();
+        this.createCompileButton();
+        this.createRunButton();
+        this.createCleanButton();
     }
 
     createBuildModeSwitch() {
@@ -32,6 +35,34 @@ class CastleStatusBar {
     updateBuildModesButtonText() {
         this._buildModesButton.text = 'CGE: [' + this._castleConfig.buildMode.name + ']';
     }
+
+    createCompileButton() {
+        this._compileButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 19);
+        this._context.subscriptions.push(this._compileButton);
+        this._compileButton.command = this._castleConfig.commandId.compileGame;
+        this._compileButton.tooltip = 'CGE: Click to compile game';
+        this._compileButton.text = '$(gear) Compile';
+        this._compileButton.show();
+    }
+
+    createRunButton() {
+        this._runButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 18);
+        this._context.subscriptions.push(this._runButton);
+        this._runButton.command = this._castleConfig.commandId.runGame;
+        this._runButton.tooltip = 'CGE: Click to run game';
+        this._runButton.text = '$(run) Run';
+        this._runButton.show();
+    }
+
+    createCleanButton() {
+        this._runButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 18);
+        this._context.subscriptions.push(this._runButton);
+        this._runButton.command = this._castleConfig.commandId.cleanGame;
+        this._runButton.tooltip = 'CGE: Click to clean game files';
+        this._runButton.text = 'Clean';
+        this._runButton.show();
+    }
+
 }
 
 module.exports = CastleStatusBar;
