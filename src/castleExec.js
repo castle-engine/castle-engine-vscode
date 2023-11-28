3f2f1e48e201a9c39cc660e4af378f673f1f586c
and3md <and3md@gmail.com>
2023-11-28 12:37:10 +0100

    More docs in castleExec.js.

--- src/castleExec.js
+++ src/castleExec.js
@@ -1,12 +1,12 @@
-
 const vscode = require("vscode");
 
 // https://stackoverflow.com/questions/30763496/how-to-promisify-nodes-child-process-exec-and-child-process-execfile-functions
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-
 /**
+ * Executes command and returns stdout as string (empty string on error, 
+ * but shows showErrorMessage()).
  * @param {string} command command to run
  * @returns {string} stdout or empty string on error
  */
@@ -34,6 +34,10 @@ async function executeCommandAndReturnValue(command) {
 	}
 }
 
+/**
+ * Executes command, shows error in vscode error message.
+ * @param {string} command command to run
+ */
 async function executeCommand(command) {
 	let options = {};
 
@@ -49,4 +53,4 @@ async function executeCommand(command) {
 	}
 }
 
-module.exports = {executeCommandAndReturnValue, executeCommand};
+module.exports = { executeCommandAndReturnValue, executeCommand };
