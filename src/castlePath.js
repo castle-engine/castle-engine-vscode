14a36aee5d5d35042806570d1ef2a72d58cf1bdd
and3md <and3md@gmail.com>
2024-01-30 15:50:12 +0100

    Remove not needed methods.

--- src/castlePath.js
+++ src/castlePath.js
@@ -1,17 +1,5 @@
 
 /**
- * Prepares path that work when it has spaces and on windows path that works with powershell
- * @param {string} path path to fix
- * @returns {string} path ready to use with vscode.ShellExecution()
- */
-function pathForShellExecute (path) {
-    if (process.platform === 'win32')
-        return path.replace(/ /g, '` ');
-    else 
-        return path.replace(/ /g, '\\ ');
-}
-
-/**
  * Prepares path that works in option.cwd in exec (it can't have "\" as first character)
  * @param {string} path path to fix
  * @returns {string} path ready to use with option.cwd
@@ -23,13 +11,4 @@ function pathForExecCommandCwd(path) {
     return path;
 }
 
-/**
- * Prepares path that works in exec() 
- * @param {string} path path to fix
- * @returns {string} path ready to use with exec()
- */
-function pathForExecCommand(path) {
-    return '\"' + path + '\"';
-}
-
-module.exports = { pathForShellExecute, pathForExecCommand, pathForExecCommandCwd };
+module.exports = { pathForExecCommandCwd };
