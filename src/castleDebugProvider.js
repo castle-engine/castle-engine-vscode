14e1b2559f83c1642f46d89880f5e487cb27555f
and3md <and3md@gmail.com>
2024-02-20 10:49:30 +0100

    Allow selecting cgedebug even when Pascal file is not active

--- src/castleDebugProvider.js
+++ src/castleDebugProvider.js
@@ -31,53 +31,58 @@ class CastleDebugProvider {
 			if (this._castleConfig.buildToolPath === '')
 				return undefined; // abort launch
 
-			const editor = vscode.window.activeTextEditor;
-			if (editor != undefined && editor.document.languageId === 'pascal') {
-				//console.log('Editor with pascal sources');
-				config.type = 'fpDebug'; // cgedebug is used only as alias for fpDebug
-				config.name = 'Debug CGE Game with fpDebug';
-				config.request = 'launch';
-				let executableName = await castleExec.executeFileAndReturnValue(this._castleConfig.buildToolPath, ['output', 'executable-name']);
-				executableName = castleExec.addExtensionToExecutableFile(executableName);
-				config.program = '${workspaceFolder}/' + executableName;
-				config.stopOnEntry = true;
-				config.workingdirectory = '${workspaceFolder}';
-				// config.log = true;
+			config.type = 'fpDebug'; // cgedebug is used only as alias for fpDebug
+			config.name = 'Debug CGE Game with fpDebug';
+			config.request = 'launch';
+			let executableName = await castleExec.executeFileAndReturnValue(this._castleConfig.buildToolPath, ['output', 'executable-name']);
+			executableName = castleExec.addExtensionToExecutableFile(executableName);
+			config.program = '${workspaceFolder}/' + executableName;
+			config.stopOnEntry = true;
+			config.workingdirectory = '${workspaceFolder}';
+			// config.log = true;
 
-				// workaround fpDebug 0.6 bug with fpdserver executable not set properly
-				if (process.platform === 'win32') {
-					let fpDebugExtPath = vscode.extensions.getExtension("cnoc.fpdebug").extensionPath;
-					if (this._castleConfig.fpcTargetCpu === 'x86_64')
-						config.fpdserver = { executable: fpDebugExtPath + '\\bin\\fpdserver-x86_64.exe' };
-					else if (this._castleConfig.fpcTargetCpu === 'i386')
-						config.fpdserver = { executable: fpDebugExtPath + '\\bin\\fpdserver-i386.exe' };
-					else {
-						return vscode.window.showInformationMessage('fpDebug supports only x86_64 and i386 architecture on windows').then(() => {
-							return undefined; // abort launch
-						});
-					}
-				} else if (process.platform === 'linux' && this._castleConfig.fpcTargetCpu !== 'x86_64')
-					return vscode.window.showInformationMessage('fpDebug supports only x86_64 architecture on linux').then(() => {
+			// workaround fpDebug 0.6 bug with fpdserver executable not set properly
+			if (process.platform === 'win32') {
+				let fpDebugExtPath = vscode.extensions.getExtension("cnoc.fpdebug").extensionPath;
+				if (this._castleConfig.fpcTargetCpu === 'x86_64')
+					config.fpdserver = { executable: fpDebugExtPath + '\\bin\\fpdserver-x86_64.exe' };
+				else if (this._castleConfig.fpcTargetCpu === 'i386')
+					config.fpdserver = { executable: fpDebugExtPath + '\\bin\\fpdserver-i386.exe' };
+				else {
+					return vscode.window.showInformationMessage('fpDebug supports only x86_64 and i386 architecture on windows').then(() => {
 						return undefined; // abort launch
 					});
+				}
+			} else if (process.platform === 'linux' && this._castleConfig.fpcTargetCpu !== 'x86_64')
+				return vscode.window.showInformationMessage('fpDebug supports only x86_64 architecture on linux').then(() => {
+					return undefined; // abort launch
+				});
 
 
-				if (this._castleConfig.buildMode === castleConfiguration.CastleBuildModes.RELEASE)
-					vscode.window.showWarningMessage('Running debuger with release build, consider to change build type.');
-				// we run compilation only when is needed
-				if (this._castleConfig.recompilationNeeded)
-					config.preLaunchTask = 'CGE: compile-cge-game-task';
-			}
+			if (this._castleConfig.buildMode === castleConfiguration.CastleBuildModes.RELEASE)
+				vscode.window.showWarningMessage('Running debuger with release build, consider to change build type.');
+			// we run compilation only when is needed
+			if (this._castleConfig.recompilationNeeded)
+				config.preLaunchTask = 'CGE: compile-cge-game-task';
 
 			if (!config.program) {
 				return vscode.window.showInformationMessage('Cannot find a program to debug').then(() => {
 					return undefined;	// abort launch
 				});
 			}
-
 			return config;
 		}
 	}
+
+	/**
+	 * This function does nothing but must be defined to show cgedebug outside Pascal files e.g. after open folder in vscode.
+	 * @param {undefined | WorkspaceFolder} folder 
+	 * @param {CancellationToken} token 
+	 */
+	provideDebugConfigurations(folder, token) {
+		console.log('provideDebugConfigurations - START');
+		console.log('provideDebugConfigurations - STOP');
+	} 
 }
 
 module.exports = CastleDebugProvider;
