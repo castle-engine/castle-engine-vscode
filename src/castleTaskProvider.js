10a0b8bdf4491a764970833c430e10b0217fae9f
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-09-22 18:30:09 +0200

    Override env for executed build tool, to add CASTLE_ENGINE_PATH and (soon, preparations here) extend PATH with FPC and Lazarus

--- src/castleTaskProvider.js
+++ src/castleTaskProvider.js
@@ -14,6 +14,66 @@ class CastleTaskProvider {
 		this.createTasks();
 	}
 
+	/* Return ShellExecutionOptions for all build tasks. */
+	buildShellExecutionOptions()
+	{
+		let overrideEnv = {
+		};
+		/*
+			Extend the environment to
+		  - define CASTLE_ENGINE_PATH, if the engine path is defined in VS Code settings
+			  (overrides build tool auto-detection).
+			- extend PATH, to let build tool find fpc / lazbuild.
+
+			Similar to what Castle Game Engine editor does when executing build tool
+		  (see https://github.com/castle-engine/castle-engine/blob/25fc71afc3a3686e44d8e162b57d83bef217cd5f/tools/castle-editor/code/editorutils.pas#L435 ).
+		*/
+		if (this._castleConfig.enginePath !== '') {
+			overrideEnv.CASTLE_ENGINE_PATH = this._castleConfig.enginePath;
+		};
+		// TODO: extend PATH with FPC and Lazarus
+		// TODO: why build tool path is not passed as full?
+		// console.log(`buildShellExecutionOptions: overrideEnv = ${overrideEnv}`);
+		// console.log('existing PATH = ' + process.env.PATH);
+		let result = { // : vscode.ShellExecutionOptions =
+			env: overrideEnv
+		};
+		return result;
+	}
+
+	/* Create vscode.ShellExecution for compile task. */
+	executionCompile()
+	{
+		return new vscode.ShellExecution(
+			this._castleConfig.buildToolPath,
+			['compile', '--mode=' + this._castleConfig.buildMode.buildTool],
+			this.buildShellExecutionOptions()
+		);
+	}
+
+	/* Create vscode.ShellExecution for "compile and run" task.
+	 * @param {boolean} recompilationNeeded - if true, we will execute compile-run, otherwise just run.
+	*/
+	executionRun(recompilationNeeded)
+	{
+		let command = recompilationNeeded ? 'compile-run' : 'run';
+		return new vscode.ShellExecution(
+			this._castleConfig.buildToolPath,
+			[command, '--mode=' + this._castleConfig.buildMode.buildTool],
+			this.buildShellExecutionOptions()
+		);
+	}
+
+	/* Create vscode.ShellExecution for clean task. */
+	executionClean()
+	{
+		return new vscode.ShellExecution(
+			this._castleConfig.buildToolPath,
+			['clean'],
+			this.buildShellExecutionOptions()
+		);
+	}
+
 	createTasks() {
 		let bestWorkspaceFolder = castlePath.bestWorkspaceFolder();
 		try {
@@ -22,7 +82,7 @@ class CastleTaskProvider {
 				bestWorkspaceFolder,
 				'compile-cge-game-task', // task name
 				'CGE', // prefix for all tasks
-				new vscode.ShellExecution(this._castleConfig.buildToolPath, ['compile', '--mode=' + this._castleConfig.buildMode.buildTool]), // what to do
+				this.executionCompile(),
 				'$cge-problem-matcher'
 			);
 			this._compileTask.group = vscode.TaskGroup.Build;
@@ -32,7 +92,7 @@ class CastleTaskProvider {
 				bestWorkspaceFolder,
 				'run-cge-game-task', // task name
 				'CGE', // prefix for all tasks
-				new vscode.ShellExecution(this._castleConfig.buildToolPath, [' compile-run', '--mode=' + this._castleConfig.buildMode.buildTool]), // what to do
+				this.executionRun(true),
 				'$cge-problem-matcher'
 			);
 
@@ -41,7 +101,7 @@ class CastleTaskProvider {
 				bestWorkspaceFolder,
 				'clean-cge-game-task', // task name
 				'CGE', // prefix for all tasks
-				new vscode.ShellExecution(this._castleConfig.buildToolPath, ['clean']), // what to do
+				this.executionClean(),
 				'$cge-problem-matcher'
 			);
 			this._cleanTask.group = vscode.TaskGroup.Clean;
@@ -54,18 +114,15 @@ class CastleTaskProvider {
 	}
 
 	/**
-	 * Updates compile task to use proper build mode and run game
-	 * task when recompilationNeeded changes o buildToolPath changes.
+	 * Updates the tasks to
+	 * - use proper build mode
+	 * - proper buildToolPath
+	 * - follow recompileNeeded flag
 	 */
 	updateCastleTasks() {
-		if (this._castleConfig.recompilationNeeded)
-			this._runTask.execution = new vscode.ShellExecution(this._castleConfig.buildToolPath, ['compile-run', '--mode=' + this._castleConfig.buildMode.buildTool]);
-		else
-			this._runTask.execution = new vscode.ShellExecution(this._castleConfig.buildToolPath, ['run', '--mode=' + this._castleConfig.buildMode.buildTool]);
-
-		this._compileTask.execution = new vscode.ShellExecution(this._castleConfig.buildToolPath, ['compile', '--mode=' + this._castleConfig.buildMode.buildTool]);
-
-		this._cleanTask.execution = new vscode.ShellExecution(this._castleConfig.buildToolPath, ['clean']);
+		this._compileTask.execution = this.executionCompile();
+		this._runTask.execution = this.executionRun(this._castleConfig.recompilationNeeded);
+		this._cleanTask.execution = this.executionClean();
 	}
 
 	/**
